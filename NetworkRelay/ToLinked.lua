2f3991b2f0fe1038f940820e27b4781322c31e54
LittleTimmy52 <cclashman21@gmail.com>
2023-01-05 22:14:35 -0500

    NetworkRelay working

--- NetworkRelay/ToLinked.lua
+++ NetworkRelay/ToLinked.lua
@@ -3,19 +3,33 @@ local serialization = require("serialization")
 local event = require("event")
 local tunnel = component.tunnel
 
-while true do
+-- set to addr of any nearby relays wireless networkcard to avoid send loop
+local relayAddr = "addr"
+
+-- set reue if using above
+local useAbove = true
+
+local function main()
 	-- gather nessicairy data
-	local _, _, _, port, _, data = event.pull("modem_message")
-	local evtDat = {}
+	local _, _, from, port, _, data = event.pull("modem_message")
+	evtDat = {}
+
+	if useAbove == true then
+		if from == relayAddr then return end
+	end
 
 	-- check if it's a wireless network message
 	if port ~= 0 then
-	-- add data to the table
+		-- add data to the table
 		table.insert(evtDat, port)
 		table.insert(evtDat, data)
 
 		-- serialize and send over the linked card
-		local serEvt = serialization.serialize(serEvt)
+		local serEvt = serialization.serialize(evtDat)
 		tunnel.send(serEvt)
 	end
+end
+
+while true do
+main()
 end
--- NetworkRelay/ToWireless.lua
+++ NetworkRelay/ToWireless.lua
@@ -3,10 +3,17 @@ local serialization = require("serialization")
 local event = require("event")
 local modem = component.modem
 
-while true do
+local function main()
+	-- gather nessicairy data
 	local _, _, _, port, _, data = event.pull("modem_message")
 	if port = 0 then
-		unserEvt = serialization.unserialize(data)
+		-- unserialize and broadcast
+		modem.open(unserEvt[1])
+		unserEvt == serialization.unserialize(data)
 		modem.broadcast(unserEvt[1], unserEvt[2])
 	end
+end
+
+while true do
+main()
 end
