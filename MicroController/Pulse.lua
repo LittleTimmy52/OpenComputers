d1c696e9b99c8f12b62e14ee3b0e07baac26e742
LittleTimmy52 <cclashman21@gmail.com>
2024-07-01 16:03:56 -0400

    ee

--- MicroController/Pulse.lua
+++ MicroController/Pulse.lua
@@ -2,13 +2,22 @@ local redstone = component.proxy(component.list("redstone")())
 local pulseLength = 60 -- 1 minute in seconds
 local delayBetweenPulses = 300 -- 5 minutes in seconds
 
+local function sleep(delay)
+	local start = os.clock()
+	while (os.clock() - start) < delay do
+		coroutine.yield()
+	end
+end
+
 local function emitPulse()
 	redstone.setOutput(0, 15)
 	redstone.setOutput(2, 15)
 	redstone.setOutput(3, 15)
 	redstone.setOutput(4, 15)
 	redstone.setOutput(5, 15)
-	os.sleep(pulseLength)
+
+	sleep(pulseLength)
+
 	redstone.setOutput(0, 0)
 	redstone.setOutput(2, 0)
 	redstone.setOutput(3, 0)
@@ -23,11 +32,11 @@ end
 local function mainLoop()
   while true do
 	if isActive() then
-	  os.sleep(delayBetweenPulses)
+	  sleep(delayBetweenPulses)
 	else
 	  -- Wait for top to be unpowered
 	  while redstone.getInput(1) == false do
-		os.sleep(0.1)
+		sleep(0.1)
 	  end
 	end
 	emitPulse()
