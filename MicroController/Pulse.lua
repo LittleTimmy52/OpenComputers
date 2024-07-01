55ff7be42b246388531f62aba73706d7f7db7958
LittleTimmy52 <cclashman21@gmail.com>
2024-07-01 16:11:33 -0400

    eeeeeeee

--- MicroController/Pulse.lua
+++ MicroController/Pulse.lua
@@ -1,6 +1,7 @@
 local redstone = component.proxy(component.list("redstone")())
 local pulseLength = 60 -- 1 minute in seconds
 local delayBetweenPulses = 300 -- 5 minutes in seconds
+local active = true
 
 local function sleep(delay)
 	local start = os.clock()
@@ -9,38 +10,28 @@ local function sleep(delay)
 	end
 end
 
-local function emitPulse()
-	redstone.setOutput(0, 15)
-	redstone.setOutput(2, 15)
-	redstone.setOutput(3, 15)
-	redstone.setOutput(4, 15)
-	redstone.setOutput(5, 15)
-
-	sleep(pulseLength)
-
-	redstone.setOutput(0, 0)
-	redstone.setOutput(2, 0)
-	redstone.setOutput(3, 0)
-	redstone.setOutput(4, 0)
-	redstone.setOutput(5, 0)
-end
-
-local function isActive()
-  return redstone.getInput(1) == false
-end
-
 local function mainLoop()
-  while true do
-	if isActive() then
-	  sleep(delayBetweenPulses)
-	else
-	  -- Wait for top to be unpowered
-	  while redstone.getInput(1) == false do
-		sleep(0.1)
-	  end
+	while true do
+		active = redstone.getInput(1) == false
+
+		if active then
+			redstone.setOutput(0, 15)
+			redstone.setOutput(2, 15)
+			redstone.setOutput(3, 15)
+			redstone.setOutput(4, 15)
+			redstone.setOutput(5, 15)
+		end
+
+		sleep(pulseLength)
+
+		redstone.setOutput(0, 0)
+		redstone.setOutput(2, 0)
+		redstone.setOutput(3, 0)
+		redstone.setOutput(4, 0)
+		redstone.setOutput(5, 0)
+
+		sleep(delayBetweenPulses)
 	end
-	emitPulse()
-  end
 end
 
 mainLoop()
